e8c53d688e581d2705e365b9c4f425551ac87735
b23064155-art <b23064155@gmail.com>
2025-11-24 14:10:11 +0500

    bilol done

--- src/components/ui/main/Sec1/Sec1.jsx
+++ src/components/ui/main/Sec1/Sec1.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './Sec1.css'
+import max from "../../../../assets/mix.png"
+import fed from "../../../../assets/fed.png"
+import goo from "../../../../assets/goo.png"
+import hub from "../../../../assets/hub.png"
+import vek from "../../../../assets/vek.png"
+import wav from "../../../../assets/wav.png"
 function Sec1() {
   return (
-    <div>
-      <h1>Bilol</h1>
+    <div className='box1' >
+      <a href="#"><img src={vek} alt="" /></a>
+      <a href="#"><img src={hub} alt="" /></a>
+      <a href="#"><img src={goo} alt="" /></a>
+      <a href="#"><img src={max} alt="" /></a>
+      <a href="#"><img src={wav} alt="" /></a>
+      <a href="#"><img src={fed} alt="" /></a>
     </div>
   )
 }
--- src/components/ui/main/Sec3/Sec3.jsx
+++ src/components/ui/main/Sec3/Sec3.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 import "./Sec3.css"
 function Sec3() {
   return (
-    <div>
-      <h1>bilol</h1>
+    <div className='box5' >
+      <div className="box2">
+        <h1>100%</h1>
+        <p>Organic Products</p>
+      </div>
+      <div className="box3">
+        <h1>2M+</h1>
+        <p>Yearly Sales</p>
+      </div>
+      <div className="box4">
+        <h1>1.9M+</h1>
+        <p>Happy Customar</p>
+      </div>
     </div>
   )
 }
